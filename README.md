b337438c9dd82a13a383c6f4c64f27bbddce7ab7
zeevmanilovich <zeev.manilovich@ironsrc.com>
2016-10-26 17:40:04 +0300

    adding pixel tracking code and example

--- README.md
+++ README.md
@@ -69,6 +69,9 @@ session.flush(null, function (results) { // Optional on-demand flush
 
 ## Change Log
 
+### V1.1.2
+- Added pixel tracking code + example
+
 ### V1.1.1
 - Improved example
 - Improved README
---
+++ atom-session-sdk/src/atom-pixel.js
@@ -0,0 +1,20 @@
+/**
+ * Tracker Factory - Returns a getTrackFrame function that's used to track data to Atom through iframe */
+function trackerFactory(trackUrl) {
+  var getTrackFrame = function (stream, data) {
+
+    if ((typeof data !== 'string' && !(data instanceof String))) {
+      try {
+        data = JSON.stringify(data);
+      } catch (e) {
+        throw new Error("data is invalid - can't be stringified")
+      }
+    }
+    var atomIframe = document.createElement('iframe');
+    atomIframe.src = trackUrl + '?stream=' + encodeURIComponent(stream) + '&data=' + encodeURIComponent(data);
+    atomIframe.height = "0px";
+    atomIframe.width = "0px";
+    document.body.appendChild(atomIframe);
+  };
+  return getTrackFrame;
+}
